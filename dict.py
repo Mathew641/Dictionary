7c0f2f409bbb36acd0e1d377b5b0ea0cd037c89d
Mathew641 <106030476+Mathew641@users.noreply.github.com>
2022-11-18 04:03:12 -0800

    Update dict.py

--- dict.py
+++ dict.py
@@ -59,28 +59,6 @@ print(dict)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #Dictionary creation and understanding
 
 # Dictionary --- Another data type in python.
@@ -153,7 +131,76 @@ print(dict['State_visited'])        # O/p -- {'Haryana', 'punjab'}
 
 print(dict['skills'])           # O/p -- C
 
+print(dict['movies'])   # o/p -('GI-Joe', 'sky') --- List
+# print secondary movie 
+print(dict['movies'][1])    # o/p -- sky
 
+
+print(dict['State_visited'])  # o/p -- {'punjab', 'Haryana'}-- Set
+#print(dict['State_visited'][1])  # o/p --- TypeError: 'set' object is not subscriptable
+
+
+print(dict['movies'][0])    # o/p -- GI-Joe
+
+print(dict['City'])         # o/p -- vidisha
+
+print(dict['other_var'])    # o/p -- {'Fruit': 'Apple', 'Animal': 'Rabit', 'Sport': 'Cricket'}   -- This is a dictionary kind of output
+
+#print(dict['other_var'][1])     # o/p -- KeyError: 1
+            # This is because the value assigned to key "other_var" is dictionary and on dictionary, method of indexing can't be applicable. 
+
+
+# with the help of indices we can access nested elements of list, tuple only but same way is not applicable on dictionary.
+
+# So let me show you how we print value of specific key from dictionary nested in some other dictionary
+print(dict['other_var']['Animal'])      # o/p -- Rabit
+
+print(dict['other_var']['Fruit'])       # o/p -- Apple
+
+print(dict['other_var']['Sport'])       # o/p --- Cricket
+
+
+
+# Let's see the syntax for updatting the distionary 
+        # Dictionar_name['Key'] = 'New_value'    This way we can assigne/update any key of dictionary by new value.
+
+print(dict)
+dict['Name'] = 'Ankita'
+print(dict)
+
+dict['Age'] = 28
+print(dict)
+
+
+dict['skills'] = 'python'
+print(dict)
+
+# Let's find out how to print only keys of our dictionary
+#print(dict('key'))              # TypeError: 'dict' object is not callable
+
+'''All_key = dict.keys()
+print(All_key)          # o/p -- dict_keys(['Name', 'Age', 'skills', 'state', 'City', 'State_visited', 'movies', 48, 'other_var'])
+'''
+
+print("All the keys used in our dictionary are: ",dict.keys())
+    # o/p --- All the keys used in our dictionary are:  dict_keys(['Name', 'Age', 'skills', 'state', 'City', 'State_visited', 'movies', 48, 'other_var'])
+
+# Similarly we can also get the all values only of dict
+print(dict.values())
+'''# o/p --- dict_values(['Ankita', 28, 'python', 'M.P.', 'vidisha', {'Haryana', 'punjab'}, 
+    # ('GI-Joe', 'sky'), 'Virat_jersey', {'Fruit': 'Apple', 'Animal': 'Rabit', 'Sport': 'Cricket'}])
+'''
+
+
+# Let's see how to calculate toatal no. of keys use in dictionary
+'''x = (int)(dict.keys())
+print(x)        # o/p -- TypeError: int() argument must be a string, a bytes-like object or a number, not 'dict_keys'
+'''
+
+# Let's see how to iterate on dictionary
+
+for k,v in dict.items():
+    print("Key is",k, 'and', 'Value is',v)
 
 
 
